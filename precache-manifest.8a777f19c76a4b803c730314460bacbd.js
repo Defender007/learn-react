27dc92ac6d0cad0b07b1500c4c009cf3e5aabaaa
Defender007 <mydjangopythonprojects@gmail.com>
2019-07-02 21:56:06 +0100

    Updates

---
+++ precache-manifest.8a777f19c76a4b803c730314460bacbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d277091c69015779c3695d40e943abf9",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "e2cb037e98e5e433c809",
+    "url": "/learn-react/static/css/main.1a942d58.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/learn-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e2cb037e98e5e433c809",
+    "url": "/learn-react/static/js/main.4899f43f.chunk.js"
+  },
+  {
+    "revision": "1f5a4aa29338276e39d8",
+    "url": "/learn-react/static/js/runtime~main.1bcabeff.js"
+  }
+]);
